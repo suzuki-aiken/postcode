f748aa1445573d8634e68cfe6ceb29872ddbbddc
suzuki-ai <karikakko10@gmail.com>
2019-09-24 11:29:43 +0900

    住所を表示するコードの作成/zipcloudを利用して住所を取得する機能実装
    ・決められた郵便番号
    
    ・request.get()の練習
    ・WebAPIを利用したJSONのパース
    ・変数の抽出　・変数のインライン化

--- postcode.py
+++ postcode.py
@@ -2,3 +2,32 @@
 WebAPI
 
 """
+import requests
+
+
+def main():
+    # 1600023 -> '東京都新宿区西新宿'
+    # 入力
+    zipcode = '1600023'
+    url = f'http://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}'
+
+    # 計算
+    # リクエスト送る　受け取ってパス　フォーマット
+
+    response = requests.get(url)
+
+    # 出力
+
+    address = response.json()['results'][0]
+    都道府県 = address['address1']  # 東京都
+    市区町村 = address['address2']  # 新宿区
+    町域 = address['address3']  # 西新宿
+
+    #
+    address = f"{都道府県}{市区町村}{町域}"
+
+    print(address)
+
+
+if __name__ == '__main__':
+    main()
